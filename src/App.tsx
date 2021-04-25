9813f8c1192c127bcfc52515858951ba6219468e
kmlee95 <kmlee95@naver.com>
2021-04-25 18:20:05 +0900

    chore : init setting

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,7 @@
-import React from 'react';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
-import Home from 'pages/Home'
+import Home from "@/pages/Home";
 
 function App() {
   return (
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,6 +1,5 @@
-import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import counterReducer from './counter/counterSlice';
-
+import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
+import counterReducer from "@/store/counter/counterSlice";
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
