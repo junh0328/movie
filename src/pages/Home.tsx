163d5a8997ef33b40f2d73c74aea5694213c6a16
이준희 <junh0328@naver.com>
2021-05-05 16:18:42 +0900

    #3 feat : 기존 fetching 데이터를 사용하여 메인페이지에 렌더링

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,7 @@
-import React from "react";
+import React, { FC, useEffect, useState } from "react";
+import axios from "axios";
+import dotenv from "dotenv";
+
 import {
   BillboardWrap,
   InfoTitleWrapper,
@@ -12,7 +15,59 @@ import logoTitle from "../images/logoTitle.png";
 import { BiInfoCircle } from "react-icons/bi";
 import { AiFillCaretRight } from "react-icons/ai";
 
-function Home() {
+dotenv.config();
+
+type Original = { id: number; name: string; backdrop_path: string };
+type Toprate = {
+  id: number;
+  name: string;
+  vote_average: number;
+  vote_count: number;
+  title: string;
+  backdrop_path: string;
+};
+
+const API_KEY = process.env.REACT_APP_API;
+const BASE_URL = `https://api.themoviedb.org/3`;
+const NetFlixOriginals = `${BASE_URL}/discover/tv?api_key=${API_KEY}&with_networks=213`;
+const TopRated = `${BASE_URL}/movie/top_rated?api_key=${API_KEY}&language=en-US`;
+
+const Home: FC = () => {
+  const [orginals, setOriginals] = useState<Original[]>([]);
+  const [topRates, setTopRates] = useState<Toprate[]>([]);
+  const [upLoad, setUpLoad] = useState<Boolean>(false);
+
+  useEffect(() => {
+    if (upLoad === false) {
+      fetchNetflixOriginals();
+      fetchTopRated();
+    }
+  }, [upLoad]);
+
+  // upLoad 를 통해서 상태관리를 하고 있는데, 한쪽만 패칭되도 보여주니, 둘다 패칭된 후에 컴포넌트를 보여줄 수 있도록 만들어야 할 듯
+  const fetchNetflixOriginals = async () => {
+    try {
+      const {
+        data: { results },
+      } = await axios.get(NetFlixOriginals);
+      setOriginals(results.slice(0, 4));
+      setUpLoad(true);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const fetchTopRated = async () => {
+    try {
+      const {
+        data: { results },
+      } = await axios.get(TopRated);
+      console.log("load TopRated datas", results);
+      setTopRates(results.slice(0, 4));
+    } catch (error) {
+      console.error(error);
+    }
+  };
   return (
     <>
       <BillboardWrap>
@@ -38,9 +93,92 @@ function Home() {
           </LogoAndTextMetaLayer>
         </InfoMetaLayer>
       </BillboardWrap>
-      <MainWrapper>안녕하세요</MainWrapper>
+
+      {/* Main 작성해야 할 부분이 이 밑입니다. */}
+
+      <MainWrapper>
+        {upLoad ? (
+          <div
+            style={{
+              display: " flex",
+              flexWrap: "wrap",
+              alignItems: "center",
+              width: "220px",
+            }}
+          >
+            <div
+              style={{
+                marginTop: 20,
+                color: "white",
+                fontSize: "1.2rem",
+                fontWeight: "bolder",
+              }}
+              onClick={fetchNetflixOriginals}
+            >
+              넷플릭스 오리지널
+            </div>
+            {orginals && (
+              <ul
+                style={{
+                  display: "flex",
+                  justifyContent: "center",
+                  listStyle: "none",
+                  paddingLeft: 0,
+                }}
+              >
+                {orginals.map((original) => (
+                  <li key={original.id}>
+                    <li>{original.name}</li>
+                    <img
+                      src={`https://image.tmdb.org/t/p/original/${original.backdrop_path}`}
+                      alt={original.name}
+                      style={{ width: 400, marginRight: 10 }}
+                    />
+                  </li>
+                ))}
+              </ul>
+            )}
+            <div
+              style={{
+                marginTop: 20,
+                color: "white",
+                fontSize: "1.2rem",
+                fontWeight: "bolder",
+              }}
+            >
+              넷플릭스 Top Rated
+            </div>
+            {topRates && (
+              <ul
+                style={{
+                  display: "flex",
+                  justifyContent: "center",
+                  paddingLeft: 0,
+                }}
+              >
+                {topRates.map((topRate) => (
+                  <li key={topRate.id} style={{ listStyle: "none" }}>
+                    <li>
+                      🌟 {topRate.vote_average}, Count with {topRate.vote_count}
+                    </li>
+
+                    <li>{topRate.title}</li>
+                    <img
+                      src={`https://image.tmdb.org/t/p/original/${topRate.backdrop_path}`}
+                      alt={topRate.name}
+                      style={{ width: 400, marginRight: 10 }}
+                    />
+                  </li>
+                ))}
+              </ul>
+            )}
+          </div>
+        ) : (
+          <div>...Loading</div>
+        )}
+      </MainWrapper>
     </>
   );
-}
+};
 
 export default Home;
