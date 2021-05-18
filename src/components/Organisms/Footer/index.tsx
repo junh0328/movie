a6b4c1907545697efb9a5448df9b7bfd6b02b718
이준희 <junh0328@naver.com>
2021-05-18 22:09:48 +0900

    #6 refactor: atoms 적용 전 Footer 해당 태그들에 대한  컴포넌트화

--- src/components/Organisms/Footer/index.tsx
+++ src/components/Organisms/Footer/index.tsx
@@ -1,53 +1,68 @@
-import { SocialLinks, MemberFooterLink, MemberFooterService, MemberFooterCopyRight, FooterWrap } from './style';
+import {
+  SocialLinks,
+  MemberFooterLink,
+  MemberFooterService,
+  MemberFooterCopyRight,
+  FooterWrap,
+  MemberFootLinkUl,
+  MemberFooterLinkLI,
+  MemberFooterCopyRightDiv,
+  FooterServiceBtn,
+  SocialLink,
+} from './style';
 import { ImFacebook2, ImInstagram, ImTwitter, ImYoutube } from 'react-icons/im';
 
 function Footer(): JSX.Element {
   return (
     <FooterWrap>
       <SocialLinks>
-        <a href="https://www.facebook.com/NetflixKR" target="_blank" rel="noreferrer">
+        <SocialLink href="https://www.facebook.com/NetflixKR" target="_blank" rel="noreferrer">
           <ImFacebook2 />
-        </a>
-        <a href="https://www.instagram.com/netflixkr/" target="_blank" rel="noreferrer">
+        </SocialLink>
+        <SocialLink href="https://www.instagram.com/netflixkr/" target="_blank" rel="noreferrer">
           <ImInstagram />
-        </a>
-        <a href="https://twitter.com/netflixkr" target="_blank" rel="noreferrer">
+        </SocialLink>
+        <SocialLink href="https://twitter.com/netflixkr" target="_blank" rel="noreferrer">
           <ImTwitter />
-        </a>
-        <a href="https://www.youtube.com/channel/UCiEEF51uRAeZeCo8CJFhGWw/featured" target="_blank" rel="noreferrer">
+        </SocialLink>
+        <SocialLink
+          href="https://www.youtube.com/channel/UCiEEF51uRAeZeCo8CJFhGWw/featured"
+          target="_blank"
+          rel="noreferrer"
+        >
           <ImYoutube />
-        </a>
+        </SocialLink>
       </SocialLinks>
       <MemberFooterLink>
-        <ul>
-          <li>자막 및 음성</li>
-          <li>음성 지원</li>
-          <li>고객 센터</li>
-          <li>기프트카드</li>
-          <li>미디어 센터</li>
-          <li>투자 정보(IR)</li>
-          <li>입사 정보</li>
-          <li>이용 약관</li>
-          <li>개인정보</li>
-          <li>법적 고지</li>
-          <li>쿠키 설정</li>
-          <li>회사 정보</li>
-          <li>문의하기</li>
-        </ul>
+        <MemberFootLinkUl>
+          <MemberFooterLinkLI>자막 및 음성</MemberFooterLinkLI>
+          <MemberFooterLinkLI>음성 지원</MemberFooterLinkLI>
+          <MemberFooterLinkLI>고객 센터</MemberFooterLinkLI>
+          <MemberFooterLinkLI>기프트카드</MemberFooterLinkLI>
+          <MemberFooterLinkLI>미디어 센터</MemberFooterLinkLI>
+          <MemberFooterLinkLI>투자 정보(IR)</MemberFooterLinkLI>
+          <MemberFooterLinkLI>입사 정보</MemberFooterLinkLI>
+          <MemberFooterLinkLI>이용 약관</MemberFooterLinkLI>
+          <MemberFooterLinkLI>개인정보</MemberFooterLinkLI>
+          <MemberFooterLinkLI>법적 고지</MemberFooterLinkLI>
+          <MemberFooterLinkLI>쿠키 설정</MemberFooterLinkLI>
+          <MemberFooterLinkLI>회사 정보</MemberFooterLinkLI>
+          <MemberFooterLinkLI>문의하기</MemberFooterLinkLI>
+        </MemberFootLinkUl>
       </MemberFooterLink>
       <MemberFooterService>
-        <button>서비스 코드</button>
+        <FooterServiceBtn>서비스 코드</FooterServiceBtn>
       </MemberFooterService>
       <MemberFooterCopyRight>
-        <div>
-          <div>
-            넷플릭스서비시스코리아 유한회사 통신판매업신고번호: 제2018-서울종로-0426호 전화번호: 00-308-321-0058
-          </div>
-          <div>대표: 레지널드 숀 톰프슨</div>
-          <div>이메일 주소: korea@netflix.com</div>
-          <div>주소: 대한민국 서울특별시 종로구 우정국로 26, 센트로폴리스 A동 20층 우편번호 03161</div>
-          <div>클라우드 호스팅: Amazon Web Services Inc.</div>
-        </div>
+        <MemberFooterCopyRightDiv>
+          넷플릭스서비시스코리아 유한회사 통신판매업신고번호: 제2018-서울종로-0426호 전화번호: 00-308-321-0058
+        </MemberFooterCopyRightDiv>
+        <MemberFooterCopyRightDiv>대표: 레지널드 숀 톰프슨</MemberFooterCopyRightDiv>
+        <MemberFooterCopyRightDiv>이메일 주소: korea@netflix.com</MemberFooterCopyRightDiv>
+        <MemberFooterCopyRightDiv>
+          주소: 대한민국 서울특별시 종로구 우정국로 26, 센트로폴리스 A동 20층 우편번호 03161
+        </MemberFooterCopyRightDiv>
+        <MemberFooterCopyRightDiv>클라우드 호스팅: Amazon Web Services Inc.</MemberFooterCopyRightDiv>
       </MemberFooterCopyRight>
     </FooterWrap>
   );
--- src/components/Organisms/Footer/style.ts
+++ src/components/Organisms/Footer/style.ts
@@ -1,5 +1,6 @@
 import styled from '@emotion/styled';
 
+// 푸터 전체를 감싸는 부분
 export const FooterWrap = styled.div`
   max-width: 980px;
   margin: 20px auto 0;
@@ -7,45 +8,59 @@ export const FooterWrap = styled.div`
   color: gray;
 `;
 
+// SNS 링크 전체를 감싸는 부분
 export const SocialLinks = styled.div`
   margin-bottom: 1rem;
-  & a {
-    font-size: 28px;
-    margin-right: 25px;
-    color: gray;
-  }
 `;
 
+export const SocialLink = styled.a`
+  font-size: 28px;
+  margin-right: 25px;
+  color: gray;
+`;
+
+// 기타 넷플릭스 페이지를 링크 해주는 부분
 export const MemberFooterLink = styled.div`
   color: gray;
   font-size: 13px;
-
-  & ul {
-    display: flex;
-    flex-direction: row;
-    flex-wrap: wrap;
-    align-items: flex-start;
-    margin: 0 0 14px;
-    padding: 0;
-    margin-bottom: 14px;
-    & li {
-      list-style: none;
-      flex-basis: 25%;
-      margin-bottom: 16px;
-    }
-  }
 `;
+
+export const MemberFootLinkUl = styled.ul`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  align-items: flex-start;
+  margin: 0 0 14px;
+  padding: 0;
+  margin-bottom: 14px;
+`;
+
+export const MemberFooterLinkLI = styled.li`
+  list-style: none;
+  flex-basis: 25%;
+  margin-bottom: 16px;
+`;
+
+// 서비스 코드 버튼을 감싸는 div
 export const MemberFooterService = styled.div`
   margin-bottom: 20px;
 
   & button {
-    padding: 0.5em;
-    border: 1px solid gray;
-    background: 0 0;
-    font-size: 13px;
   }
 `;
 
+// 서비스 코드 버튼을 감싸는 div 내부 버튼
+export const FooterServiceBtn = styled.button`
+  padding: 0.5em;
+  border: 1px solid gray;
+  background: 0 0;
+  font-size: 13px;
+  cursor: pointer;
+`;
+
+// 카피라이트 wrapper
 export const MemberFooterCopyRight = styled.div`
   padding-bottom: 15px;
 `;
+
+export const MemberFooterCopyRightDiv = styled.div``;
