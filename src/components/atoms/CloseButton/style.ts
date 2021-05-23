9fd06f4d7a96482acf180633a6599e189b948569
jeje01 <jje141@gmail.com>
2021-05-23 13:48:39 +0900

    버튼 추가

---
+++ src/components/atoms/CloseButton/style.ts
@@ -0,0 +1,15 @@
+import styled from '@emotion/styled';
+
+export const Button = styled.button`
+  color: white;
+  background-color: #181818;
+  width: 30px;
+  height: 30px;
+  border-radius: 15px;
+  position: absolute;
+  top: 10px;
+  right: 10px;
+  border: none;
+  opacity: 0.8;
+  font-weight: 600;
+`;
