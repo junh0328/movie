ce7ac62b53ab79c2dc34f15201628825b31647b2
이준희 <junh0328@naver.com>
2021-06-11 22:10:34 +0900

    #18 feat: 스크롤 이벤트 발생시 데이터 패칭로직 구현 성공

--- src/pages/Latest.tsx
+++ src/pages/Latest.tsx
@@ -2,32 +2,27 @@ import { FC, useEffect, useState } from 'react';
 import { MainWrapper, SliderName } from './style';
 import { SliderContainer, SliderItem } from '@/components/organisms/Slider';
 import { ContentDetail } from '@/types/common';
-import { Action, ScienceFiction, Documentary, Horror, NetFlixMovieOriginals, Romance, Animation } from '@/apis';
+import {
+  Action,
+  ScienceFiction,
+  Documentary,
+  Horror,
+  NetFlixMovieOriginals,
+  Romance,
+  Animation,
+  Western,
+  War,
+  Family,
+  Fantasy,
+  Music,
+} from '@/apis';
 import useMovieFetch from '@/hooks/useMovieFetch';
 
 const Latest: FC = () => {
-  const [upLoad, setUpLoad] = useState(false);
+  const [upLoad, setUpLoad] = useState<boolean>(false);
+  const [seconUpload, setSecondUpload] = useState<boolean>(false);
 
-  // 무한 스크롤을 위한 스크롤 이벤트 함수 만들기 (분리 전)
-  useEffect(() => {
-    if (upLoad) {
-      console.log('지금부터 스크롤이 작동합니다');
-      // 업로드 완료 상황을 기점으로 해당 함수를 사용할 수 있도록 만듬
-
-      const onScroll = () => {
-        const scrollHeight = document.documentElement.scrollHeight;
-        const scrollTop = document.documentElement.scrollTop;
-        const clientHeight = document.documentElement.clientHeight;
-
-        if (scrollTop + clientHeight >= scrollHeight) {
-          console.log('scrolled!');
-        }
-      };
-
-      window.addEventListener('scroll', onScroll);
-    }
-  }, [upLoad]);
-
+  // previous
   const Animations = useMovieFetch<ContentDetail[]>(Animation, 'GET');
   const netFlixMovieOriginals = useMovieFetch<ContentDetail[]>(NetFlixMovieOriginals, 'GET');
   const action = useMovieFetch<ContentDetail[]>(Action, 'GET');
@@ -35,6 +30,14 @@ const Latest: FC = () => {
   const documentary = useMovieFetch<ContentDetail[]>(Documentary, 'GET');
   const horror = useMovieFetch<ContentDetail[]>(Horror, 'GET');
   const romance = useMovieFetch<ContentDetail[]>(Romance, 'GET');
+
+  // next
+  const WesternDatas = useMovieFetch<ContentDetail[]>(Western, 'GET');
+  const WarDatas = useMovieFetch<ContentDetail[]>(War, 'GET');
+  const FamilyDatas = useMovieFetch<ContentDetail[]>(Family, 'GET');
+  const FantasyDatas = useMovieFetch<ContentDetail[]>(Fantasy, 'GET');
+  const MusicDatas = useMovieFetch<ContentDetail[]>(Music, 'GET');
+  // etc ...
 
   useEffect(() => {
     if (upLoad === false) {
@@ -57,6 +60,36 @@ const Latest: FC = () => {
       ]).then(() => {
         setUpLoad(true);
       });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  // 무한 스크롤을 위한 스크롤 이벤트 함수 만들기 (분리 전)
+  useEffect(() => {
+    if (upLoad) {
+      console.log('지금부터 스크롤이 작동합니다');
+      // 업로드 완료 상황을 기점으로 해당 함수를 사용할 수 있도록 만듬
+
+      const onScroll = () => {
+        const scrollHeight = document.documentElement.scrollHeight;
+        const scrollTop = document.documentElement.scrollTop;
+        const clientHeight = document.documentElement.clientHeight;
+
+        if (scrollTop + clientHeight >= scrollHeight) {
+          console.log('scrolled!');
+          fetchMoreApis();
+          setSecondUpload(true);
+        }
+      };
+
+      window.addEventListener('scroll', onScroll);
+    }
+  }, [upLoad]);
+
+  const fetchMoreApis = async () => {
+    try {
+      await Promise.all([WesternDatas, WarDatas, FamilyDatas, FantasyDatas, MusicDatas]);
     } catch (error) {
       console.error(error);
     }
@@ -137,6 +170,60 @@ const Latest: FC = () => {
               </SliderContainer>
             )}
           </div>
+          {seconUpload && (
+            <>
+              <div>
+                <SliderName>서부 장르</SliderName>
+                {WesternDatas && (
+                  <SliderContainer>
+                    {WesternDatas.map((movie) => {
+                      return <SliderItem key={movie.id} movie={movie} />;
+                    })}
+                  </SliderContainer>
+                )}
+              </div>
+              <div>
+                <SliderName>전쟁 장르</SliderName>
+                {WarDatas && (
+                  <SliderContainer>
+                    {WarDatas.map((movie) => {
+                      return <SliderItem key={movie.id} movie={movie} />;
+                    })}
+                  </SliderContainer>
+                )}
+              </div>
+              <div>
+                <SliderName>가족 장르</SliderName>
+                {FamilyDatas && (
+                  <SliderContainer>
+                    {FamilyDatas.map((movie) => {
+                      return <SliderItem key={movie.id} movie={movie} />;
+                    })}
+                  </SliderContainer>
+                )}
+              </div>
+              <div>
+                <SliderName>판타지 장르</SliderName>
+                {FantasyDatas && (
+                  <SliderContainer>
+                    {FantasyDatas.map((movie) => {
+                      return <SliderItem key={movie.id} movie={movie} />;
+                    })}
+                  </SliderContainer>
+                )}
+              </div>
+              <div>
+                <SliderName>음악 장르</SliderName>
+                {MusicDatas && (
+                  <SliderContainer>
+                    {MusicDatas.map((movie) => {
+                      return <SliderItem key={movie.id} movie={movie} />;
+                    })}
+                  </SliderContainer>
+                )}
+              </div>
+            </>
+          )}
         </MainWrapper>
       ) : (
         <h1 style={{ display: 'flex', justifyContent: 'center', color: 'white' }}>데이터 로딩중...</h1>
