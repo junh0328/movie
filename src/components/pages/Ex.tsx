4cb76921b5d3bb0a7dd97d00c5266dcb6e357027
이준희 <junh0328@naver.com>
2021-07-01 15:29:18 +0900

    [#24] feat: reducer initialState 관련 로직 추가

--- src/components/pages/Ex.tsx
+++ src/components/pages/Ex.tsx
@@ -1,39 +1,59 @@
+import { Music } from '@/apis';
 import { RootState } from '@/reducers';
-import { fetchingMoviesRequest } from '@/reducers/movies';
+import { FETCHING_ADVENTURE_REQUEST, FETCHING_HORROR_REQUEST, FETCHING_MOVIES_REQUEST } from '@/reducers/movies';
 import React from 'react';
+import { useState } from 'react';
 import { useEffect } from 'react';
-import { useCallback } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { SliderContainer, SliderItem } from '../organisms/Slider';
+import Main from '../organisms/Main';
 import { MainWrapper } from './style';
 
 const Ex = () => {
-  const { movie } = useSelector((state: RootState) => state.movies);
+  const { fetchAdventureSuccess, fetchHorrorSuccess, fetchMovieSuccess, adventure, music, horror } = useSelector(
+    (state: RootState) => state.movies,
+  );
   const dispatch = useDispatch();
-  const callMovie = useCallback(() => {
-    if (!movie.length) {
-      dispatch(fetchingMoviesRequest());
-    }
-  }, [movie.length]);
+  const [fetchingDone, setFetchingDone] = useState(false);
 
   useEffect(() => {
-    console.log('movie 배열 감지: ', movie);
-  }, [movie]);
+    if (!fetchingDone) {
+      console.log('fetchingDone 호출!');
+      fetchAPIs();
+    }
+  }, [fetchingDone]);
+
+  useEffect(() => {
+    if (fetchAdventureSuccess && fetchHorrorSuccess && fetchMovieSuccess) {
+      console.log('Done it!');
+      setFetchingDone(true);
+    }
+  }, [fetchAdventureSuccess, fetchHorrorSuccess, fetchMovieSuccess]);
+
+  const fetchAPIs = () => {
+    dispatch({
+      type: FETCHING_ADVENTURE_REQUEST,
+    });
+    dispatch({
+      // data 없이 보내기
+      type: FETCHING_HORROR_REQUEST,
+    });
+    dispatch({
+      // data를 포함해서 보내기
+      type: FETCHING_MOVIES_REQUEST,
+      data: Music,
+    });
+  };
 
   return (
     <>
-      <div style={{ marginTop: 200 }}>
-        <button style={{ color: 'black' }} onClick={callMovie}>
-          영화 데이터 호출하기
-        </button>
-      </div>
+      <div style={{ marginTop: 200 }}></div>
       <MainWrapper>
-        {movie.length ? (
-          <SliderContainer>
-            {movie.map((m) => {
-              return <SliderItem key={m.id} movie={m} />;
-            })}
-          </SliderContainer>
+        {fetchingDone ? (
+          <div>
+            <Main name={'music'} genres={music} />
+            <Main name={'어드벤처'} genres={adventure} />
+            <Main name={'호러'} genres={horror} />
+          </div>
         ) : (
           <div>영화 데이터 없음</div>
         )}
--- src/sagas/movie/index.tsx
+++ src/sagas/movie/index.tsx
@@ -1,14 +1,24 @@
 import { all, call, fork, put, takeEvery } from 'redux-saga/effects';
-import { imssi, Animation } from '@/apis';
+import { Adventure, Horror } from '@/apis';
 import { ContentDetail } from '@/types/common';
-import { FETCHING_MOVIES_FAILURE, FETCHING_MOVIES_REQUEST, FETCHING_MOVIES_SUCCESS } from '@/reducers/movies';
+import {
+  FETCHING_ADVENTURE_FAILURE,
+  FETCHING_ADVENTURE_REQUEST,
+  FETCHING_ADVENTURE_SUCCESS,
+  FETCHING_HORROR_FAILURE,
+  FETCHING_HORROR_REQUEST,
+  FETCHING_HORROR_SUCCESS,
+  FETCHING_MOVIES_FAILURE,
+  FETCHING_MOVIES_REQUEST,
+  FETCHING_MOVIES_SUCCESS,
+} from '@/reducers/movies';
 import axios from 'axios';
 
 // 내부에서 훅 함수를 못씀 (useMovieFetch)
-async function fetchMoviesAPI() {
+async function fetchMoviesAPI(data: string) {
   try {
-    const response = await axios.get(Animation);
-    console.log(response);
+    const response = await axios.get(data);
+    // console.log(response.data);
     const result = response.data.results;
     return result;
   } catch (err) {
@@ -20,10 +30,9 @@ type resultType = {
   result: ContentDetail[];
 };
 
-function* fetchMovies() {
+function* fetchMovies(action: any) {
   try {
-    console.log('fetchMovie 들어옴');
-    const result: resultType = yield call(fetchMoviesAPI);
+    const result: resultType = yield call(fetchMoviesAPI, action.data);
     yield put({
       type: FETCHING_MOVIES_SUCCESS,
       data: result,
@@ -37,10 +46,70 @@ function* fetchMovies() {
   }
 }
 
+async function fetchAdventureAPI() {
+  try {
+    const response = await axios.get(Adventure);
+    // console.log('adventure: ', response.data.results);
+    const result = response.data.results;
+    return result;
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+function* fetchAdventure() {
+  try {
+    const result: resultType = yield call(fetchAdventureAPI);
+    yield put({
+      type: FETCHING_ADVENTURE_SUCCESS,
+      data: result,
+    });
+  } catch (err) {
+    console.log(err);
+    yield put({
+      type: FETCHING_ADVENTURE_FAILURE,
+      error: err.response,
+    });
+  }
+}
+
+async function fetchHorrorAPI() {
+  try {
+    const response = await axios.get(Horror);
+    // console.log('Horror: ', response.data.results);
+    const result = response.data.results;
+    return result;
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+function* fetchHorror() {
+  try {
+    const result: resultType = yield call(fetchHorrorAPI);
+    yield put({
+      type: FETCHING_HORROR_SUCCESS,
+      data: result,
+    });
+  } catch (err) {
+    console.log(err);
+    yield put({
+      type: FETCHING_HORROR_FAILURE,
+      error: err.response,
+    });
+  }
+}
+
 function* watchFetchMovies() {
   yield takeEvery(FETCHING_MOVIES_REQUEST, fetchMovies);
 }
+function* watchFetchAdventure() {
+  yield takeEvery(FETCHING_ADVENTURE_REQUEST, fetchAdventure);
+}
+function* watchFetchHorror() {
+  yield takeEvery(FETCHING_HORROR_REQUEST, fetchHorror);
+}
 
 export default function* movieSaga() {
-  yield all([fork(watchFetchMovies)]);
+  yield all([fork(watchFetchMovies), fork(watchFetchAdventure), fork(watchFetchHorror)]);
 }
